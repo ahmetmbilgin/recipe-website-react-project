07baf407b7aa2ef5d7a2d68c920e7c8d8d3b8c40
ahmetmbilgin <ahmet.m.bilgin@hotmail.com>
2023-01-04 09:32:25 +0300

    düzenleme #21

--- src/RestApi.js
+++ src/RestApi.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const url = 'http://localhost:4600/users/';
+const url = 'http://localhost:4600/';
 
 class RestApi {
     async getUser(username) {
-        await axios.get(url + username);
+        await axios.get(`${url}/user/${username}`);
     }
 
     async getAllUsers() {
-        return await axios.get(url);
+        return await axios.get(`${url}/user`);
     }
 
     async saveUser(userObject) {
-        await axios.post(url, userObject);
+        await axios.post(`${url}/user`, userObject);
     }
 }
 export default new RestApi();

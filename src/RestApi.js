5be3620e80ee9e38dfcbde4973ab66f9894e075e
ahmetmbilgin <ahmet.m.bilgin@hotmail.com>
2023-01-11 15:01:18 +0300

    Düzeltme

--- src/RestApi.js
+++ src/RestApi.js
@@ -4,36 +4,36 @@ const url = 'http://localhost:4600';
 
 class RestApi {
 
-    async getUser(id) {
-        return await axios.get(`${url}/users/${id}`);
+    getUser(id) {
+        return axios.get(`${url}/users/${id}`);
     }
 
-    async getReceipe(foodType) {
-        return await axios.get(`${url}/${foodType}`);
+    getReceipe(foodType) {
+        return axios.get(`${url}/${foodType}`);
     }
 
-    async getAllUsers() {
-        return await axios.get(`${url}/users`);
+    getAllUsers() {
+        return axios.get(`${url}/users`);
     }
 
-    async saveUser(userObject) {
-        await axios.post(`${url}/users`, userObject);
+    saveUser(userObject) {
+        axios.post(`${url}/users`, userObject);
     }
 
-    async changeUser(userObject, id) {
-        await axios.put(`${url}/users/${id}`, userObject);
+    changeUser(userObject, id) {
+        axios.put(`${url}/users/${id}`, userObject);
     }
 
-    async saveReceipe(receipeObject, type) {
-        await axios.post(`${url}/${type}`, receipeObject);
+    saveReceipe(receipeObject, type) {
+        axios.post(`${url}/${type}`, receipeObject);
     }
 
-    async changeReceipe(foodtype, id, receipeObject) {
-        await axios.put(`${url}/${foodtype}/${id}`, receipeObject);
+    changeReceipe(foodtype, id, receipeObject) {
+        axios.put(`${url}/${foodtype}/${id}`, receipeObject);
     }
 
-    async deleteReceipe(foodtype, id) {
-        await axios.delete(`${url}/${foodtype}/${id}`);
+    deleteReceipe(foodtype, id) {
+        axios.delete(`${url}/${foodtype}/${id}`);
     }
 
 }

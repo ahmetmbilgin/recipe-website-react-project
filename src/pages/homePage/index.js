6c1df9e2657bec52e9479ab87409db3ec65bd4d2
ahmetmbilgin <ahmet.m.bilgin@hotmail.com>
2023-01-24 23:07:00 +0300

    Anasayfa Düzenleme #18

--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import './style.scss';
+import Ahmet from '../../images/ahmet.jpg';
+
+const stil = {
+
+}
 
 const HomePage = () => {
+
+
     return (
         <>
-            <h1>Welcome to Home Page</h1>
+            <div style={{ backgroundImage: `url(${require("../../images/homepage.jpeg")})` }} className="main">
+
+                <div className="container">
+                    <h1>Simple and Tasty Recipes</h1>
+                    <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, voluptates.</h2>
+                    <div>
+                        <img src={Ahmet} alt="" />
+                        <span>by Ahmet Muhiddin Bilgin</span>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
